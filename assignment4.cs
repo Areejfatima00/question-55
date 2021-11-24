4b54fdbd5e4afdf2dc1e1e682b363f9c39b7a121
Areej Fatima <areej4761@gmail.com>
2021-11-24 23:02:22 +0500

    first commit

---
+++ assignment4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace question_55
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Value of  result / = 2");
+            Console.ReadLine();
+        }
+    }
+}
